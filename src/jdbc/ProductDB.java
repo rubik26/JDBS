45c275339ec83f5032b1d66af1e5c290bdf4b1b6
fdxgvfdsxgvdfgv <darpasanrubik32@gmail.com>
2024-02-08 22:42:54 +0400

    update

--- src/jdbc/ProductDB.java
+++ src/jdbc/ProductDB.java
@@ -1,49 +1,72 @@
 package jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class ProductDB {
     public static void main(String[] args) throws SQLException {
         try {
-            createDbProductTable();
-        } catch (SQLException e) {
+            Product phone = new Product();
+            phone.setId(1);
+            phone.setName("Phone");
+            phone.setPrice(35000);
+
+            Product laptop = new Product();
+            laptop.setId(2);
+            laptop.setName("laptop");
+            laptop.setPrice(50000);
+            Connection connection = getDbConnection();
+            createProduct(connection, phone);
+            createProduct(connection, laptop);
+            updatePrice(connection);
+        }catch (SQLException e){
             throw new SQLException(e);
         }
-
-
     }
-
-    private static String url = "jdbc:postgresql://localhost:5432/postgres";
+    private static String url = "jdbc:postgresql://localhost:5432/postgres?serverTimezoneEurope/Yerevan";
     private static String username = "postgres";
     private static String password = "root";
 
     private static Connection getDbConnection() throws SQLException {
         Connection dbConnection = null;
-        try {
+        try{
             dbConnection = DriverManager.getConnection(url, username, password);
-        } catch (SQLException e) {
+        }catch (SQLException e){
             throw new RuntimeException(e);
         }
         return dbConnection;
     }
+    protected static int createProduct(Connection connection, Product product) throws SQLException{
+        try {
+            String createProduct = "INSERT INTO product(Id, productName, price) VALUES(?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(createProduct);
+            preparedStatement.setInt(1, product.getId());
+            preparedStatement.setString(2, product.getName());
+            preparedStatement.setInt(3, product.getPrice());
 
-    private static void createDbProductTable() throws SQLException {
-        //String createTableProductSQL = "CREATE TABLE Product (Id INT PRIMARY KEY, ProductName Varchar(20), Price INT)";
-        //String createTableOrderSQL = "CREATE TABLE ordersJava (Id INT PRIMARY KEY, ProductCount INT, Price INT)";
-        //String selectProductsInSQL = "INSERT INTO ordersJava (Id, ProductCount, Price) VALUES ((SELECT (Id) FROM product WHERE ProductName = 'Audi'), 4, (SELECT (Price) FROM product WHERE ProductName = 'Audi'))";
-        // String ProductsCountsSQL = "SELECT * FROM product JOIN ordersJava OJ on Product.Id = OJ.ID WHERE OJ.ProductCount = (SELECT SUM(ProductCount) FROM ordersJava)";
-        //String ProductsAVGPriceSQL = "SELECT(AVG(Price))FROM Product";
+            preparedStatement.setInt(3, product.getPrice());
+            return preparedStatement.executeUpdate();
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+        return 0;
+    }
+    protected static int updatePrice(Connection connection) throws SQLException{
+        try{
+            String updatePrice = "UPDATE product SET Price = Price + 5000 WHERE ProductName ='Apple Vision Pro'";
+            PreparedStatement preparedStatement = connection.prepareStatement(updatePrice);
+            preparedStatement.executeUpdate(updatePrice);
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+        return 0;
+    }
+    private static void createDbProductTable() throws SQLException{
+        String createTableSQL = "CREATE TABLE product (Id INT PRIMARY KEY, ProductName VARCHAR(20), Price INT)";
+
 
         Connection connection = getDbConnection();
         Statement statement = connection.createStatement();
-        //statement.execute(createTableProductSQL)
-        //statement.execute(createTableOrderSQL);
-        //statement.execute(selectProductsInSQL);
-        //statement.execute(ProductsCountsSQL);
-        //statement.execute((ProductsAVGPriceSQL));
+        statement.execute(createTableSQL);
         connection.close();
     }
 }
